24ba2238311f48b88a702eaaafe94b9041eabf32
Alvin Mukiibi <mukikelly792@gmail.com>
2020-01-22 15:27:47 +0300

    small change to dms feature

--- resources/js/components/FileManager.vue
+++ resources/js/components/FileManager.vue
@@ -1,56 +1,58 @@
 <template>
-  <div>
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Document Manager</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item">
-                <a href="#">Home</a>
-              </li>
-              <li class="breadcrumb-item active">Document Manager</li>
-            </ol>
-          </div>
-        </div>
-      </div>
-    </div>
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="card-outline card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Document Manager</h3>
-              </div>
-              <div class="card-body">
-                <div id="fm" style="height:400px">
-                  <file-manager :settings="settings"></file-manager>
+    <div>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">Document Manager</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item">
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="breadcrumb-item active">
+                                Document Manager
+                            </li>
+                        </ol>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </section>
-  </div>
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card-outline card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">Document Manager</h3>
+                            </div>
+                            <div class="card-body">
+                                <div id="fm" style="height:400px">
+                                    <file-manager
+                                        :settings="settings"
+                                    ></file-manager>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      settings: {
-        headers: {},
-        baseUrl: "http://youth.local/file-manager",
-        windowsConfig: 2
-      }
-    };
-  }
+    data() {
+        return {
+            settings: {
+                headers: {},
+                windowsConfig: 2
+            }
+        };
+    }
 };
 </script>
 
-<style >
-</style>
+<style></style>
